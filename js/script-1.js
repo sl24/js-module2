d134c2f8575a58a05e7188297f988ab472866bcb
sl24 <slvitalik24@gmail.com>
2020-07-31 12:08:48 +0300

    Добавляет решение 1й задачи

--- js/script-1.js
+++ js/script-1.js
@@ -1,11 +1,38 @@
-const name = "«Генератор защитного поля»"; // Write code on this line
-let price = 1000; // Write code on this line
-const firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`; // Write code on this line
-price = 2000;
-const secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов`; // Write code on this line
+const getItemsString = function (array) {
+  // Write code under this line
 
-console.log(firstMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+  let result = "";
 
-console.log(secondMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+
+  return result;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+//console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
+
+// Использование for
+// Напиши функцию getItemsString(array), которая получает массив и возвращает строку, полученную из объединения(конкатенации) строк в формате ${ номер элемента } - ${ значение элемента } \n, где \n - спецсимвол переноса.
+
+// Нумерация должна начинаться с 1. К примеру для первого элемента массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
+
+// Используйте вспомогательную переменную result для добавления(конкатенации) строк внутри цикла for
