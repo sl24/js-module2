0ed9f0ac4132f935ff4645fbc44176328035e61b
sl24 <slvitalik24@gmail.com>
2020-08-02 14:46:58 +0300

    Добавил условие 5й задачи

--- js/script-5.js
+++ js/script-5.js
@@ -14,3 +14,11 @@ console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
 
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.Если нашли запрещенное слово
+// то функция возвращает true, если запрещенных слов нет функция возвращает false.
+// Слова в строке могут быть в произвольном регистре.
